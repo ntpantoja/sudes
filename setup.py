0f94892c025a6376d6324d46ad31d87899f89aa7
ntpantoja <154106208+ntpantoja@users.noreply.github.com>
2023-12-19 16:44:29 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='uber_pickups.py',
+    version='0.1',
+    install_requires=[
+        'matplotlib==3.4.3',
+        # Outras dependências
+    ],
+)
